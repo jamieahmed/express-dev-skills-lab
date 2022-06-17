2233f470a2f309bd45e3760fae4cb30b0f30b8ca
Jamie Ahmed <jamieahmed273@gmail.com>
2022-06-17 08:11:51 -0400

    express-dev-skills-lab done

---
+++ models/skill.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema
+
+const skillSchema = new Schema({
+    text: String,
+    done: Boolean,
+})
+
+// Compile the schema into a model and export it
+const Skill = mongoose.model('Skill', skillSchema)
+
+export {
+    Skill
+}
--- server.js
+++ server.js
@@ -1,12 +1,17 @@
+import "dotenv/config.js"
 import createError from 'http-errors'
 import express from 'express'
 import path from 'path'
 import { fileURLToPath } from 'url'
 import logger from 'morgan'
+import methodOverride from 'method-override'
+// Connect to the database with Mongoose
+import('./config/database.js')
+
 
 // import routers
 import { router as indexRouter } from './routes/index.js'
-import { router as usersRouter } from './routes/users.js'
+import { router as skillsRouter } from './routes/skills.js'
 
 // set up app
 const app = express()
@@ -19,6 +24,10 @@ app.set(
 app.set('view engine', 'ejs')
 
 // middleware
+app.use(function (req, res, next) {
+  req.time = new Date().toLocaleTimeString()
+  next()
+})
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
@@ -27,10 +36,10 @@ app.use(
     path.join(path.dirname(fileURLToPath(import.meta.url)), 'public')
   )
 )
-
+app.use(methodOverride('_method'))  // for override method
 // mounted routers
 app.use('/', indexRouter)
-app.use('/users', usersRouter)
+app.use('/skills', skillsRouter)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
